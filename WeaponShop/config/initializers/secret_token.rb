4993e58bd4fdadd136b4788a4abf4dd0ca777fb8
Ivan Gorbulin <gorbulin92@gmail.com>
2014-03-07 15:15:06 +0600

    Создан проект на Ruby On Rails (RoR)

---
+++ WeaponShop/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeaponShop::Application.config.secret_key_base = 'd5ee9d7476f62bcd996bb08574c7f53339ad83275581408781c30f3279322b296f74b28c18d6223e3f095076bd594890a53111712ab9432b2e88ef46b231b745'
